7b2ae0bdfa0b3335301423499824f9a54f2c0d7d
AshnaMg <ashnamg1998@gmail.com>
2024-01-10 14:04:17 +0530

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, reset } from '../redux/counterSlice';
+
+function Counter() {
+  const [range, setRange]= useState(1)
+  const dispatch = useDispatch();
+  const count = useSelector(state=>state.counter.value);
+  return (
+    <div className='d-flex align-items-center justify-content-center w-100 mt-2 flex-column'>
+        <h1 style={{fontSize:"70px",color:"black"}}>{count}</h1>
+        <div>
+            <button className='btn btn-danger p-3 ms-2' onClick={()=>dispatch(decrement(Number(range)))}>Decrement</button>
+            <button className='btn btn-success p-3 ms-2' onClick={()=>dispatch(increment(Number(range)))}>Increment</button>
+            <button className='btn btn-primary p-3 ms-2' onClick={()=>dispatch(reset())}  style={{width:"120px"}}>Reset</button>
+        </div>
+        <div>
+          <input type="number" className='form-control w-100 mt-5 border border-primary' placeholder='Enter increment/decrement range' style={{minWidth:"300px"}}
+          onChange={(e)=>setRange(e.target.value)}
+          />
+        </div>
+    </div>
+
+  )
+}
+
+export default Counter
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import counterSlice from "./counterSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+reducer:{
+    counter:counterSlice
+}
+})
